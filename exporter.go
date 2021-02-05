7ced8464e05a2453de6763e1d57330108b4f2cef
Hector <dev@hsmith.org>
2021-02-05 23:01:00 +0000

    feat: initial setup of metric exporter
    Add dependency on the prometheus library to start exporting metrics. Add
    a new `up` metric that always returns `1` to ensure the exporter is
    working as expected.

--- exporter.go
+++ exporter.go
@@ -1,7 +1,39 @@
 package main
 
-import "log"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+const namespace = "fail2ban"
+
+var up = prometheus.NewDesc(
+	prometheus.BuildFQName(namespace, "", "up"),
+	"Was the last fail2ban query successful.",
+	nil, nil,
+)
+
+type Exporter struct {
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- up
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	ch <- prometheus.MustNewConstMetric(
+		up, prometheus.GaugeValue, 1,
+	)
+}
 
 func main() {
-	log.Println("Not yet implemented")
+	log.Print("starting fail2ban exporter")
+
+	exporter := &Exporter{}
+	prometheus.MustRegister(exporter)
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9101", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module fail2ban-prometheus-exporter
 
 go 1.15
+
+require github.com/prometheus/client_golang v1.9.0
