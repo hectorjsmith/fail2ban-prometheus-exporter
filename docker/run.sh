b268f8654c7f4738889641abd409652b6e57f277
Hector <dev@hsmith.org>
2021-10-15 18:02:26 +0000

    remove: database-based metrics
    Remove all database-based metrics from the metrics endpoint.
    Remove all code related to pulling metrics from the fail2ban database.
    Remove all configuration variables related to the fail2ban database.
    The CLI parameter for the database path was not removed to avoid breaking
    compatibility.
    Update docker entrypoint to remove references to the fail2ban database.
    Remove all references to the old database metrics from the README.

--- docker/run.sh
+++ docker/run.sh
@@ -3,18 +3,11 @@
 # Print version to logs for debugging purposes
 /app/fail2ban-prometheus-exporter -version
 
-db_path=/app/fail2ban.sqlite3
 socket_path=/var/run/fail2ban/fail2ban.sock
 textfile_dir=/app/textfile/
 textfile_enabled=false
 
-# Blank out the file paths if they do not exist - a hacky way to only use these files if they were mounted into the container.
-if [ ! -f "$db_path" ]; then
-    db_path=""
-fi
-if [ ! -S "$socket_path" ]; then
-    socket_path=""
-fi
+# Enable textfile metrics if the folder exists (i.e. was mounted by docker)
 if [ -d $textfile_dir ]; then
     textfile_enabled=true
 fi
@@ -22,7 +15,6 @@ fi
 # Start the exporter (use exec to support graceful shutdown)
 # Inspired by: https://akomljen.com/stopping-docker-containers-gracefully/
 exec /app/fail2ban-prometheus-exporter \
-    -db "$db_path" \
     -socket "$socket_path" \
     -collector.textfile=$textfile_enabled \
     -collector.textfile.directory="$textfile_dir"
--- src/cfg/cfg.go
+++ src/cfg/cfg.go
@@ -14,7 +14,6 @@ const (
 type AppSettings struct {
 	VersionMode          bool
 	MetricsPort          int
-	Fail2BanDbPath       string
 	Fail2BanSocketPath   string
 	FileCollectorPath    string
 	FileCollectorEnabled bool
@@ -24,10 +23,12 @@ func Parse() *AppSettings {
 	appSettings := &AppSettings{}
 	flag.BoolVar(&appSettings.VersionMode, "version", false, "show version info and exit")
 	flag.IntVar(&appSettings.MetricsPort, "port", 9191, "port to use for the metrics server")
-	flag.StringVar(&appSettings.Fail2BanDbPath, "db", "", "path to the fail2ban sqlite database (deprecated)")
 	flag.StringVar(&appSettings.Fail2BanSocketPath, "socket", "", "path to the fail2ban server socket")
 	flag.BoolVar(&appSettings.FileCollectorEnabled, "collector.textfile", false, "enable the textfile collector")
 	flag.StringVar(&appSettings.FileCollectorPath, "collector.textfile.directory", "", "directory to read text files with metrics from")
+
+	// deprecated: to be removed in next version
+	_ = flag.String("db", "", "path to the fail2ban sqlite database (removed)")
 
 	flag.Parse()
 	appSettings.validateFlags()
@@ -37,8 +38,8 @@ func Parse() *AppSettings {
 func (settings *AppSettings) validateFlags() {
 	var flagsValid = true
 	if !settings.VersionMode {
-		if settings.Fail2BanDbPath == "" && settings.Fail2BanSocketPath == "" {
-			fmt.Println("at least one of the following flags must be provided: 'db', 'socket'")
+		if settings.Fail2BanSocketPath == "" {
+			fmt.Println("fail2ban socket path must not be blank")
 			flagsValid = false
 		}
 		if settings.MetricsPort < minServerPort || settings.MetricsPort > maxServerPort {
